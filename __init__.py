881872db707b834a22a81a058e305deaf0986f2c
Artem Dmitriev <77476430+crexlight@users.noreply.github.com>
2025-12-27 19:48:35 +0900

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .downloader import ModelDownloader
+
+NODE_CLASS_MAPPINGS = {
+    "ModelDownloader": ModelDownloader
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ModelDownloader": "📥 Model Downloader"
+}
